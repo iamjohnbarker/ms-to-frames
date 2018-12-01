205e31a12954f9e0c38a6dc41385264e0611772c
John Barker <iamjohnbarker@hotmail.com>
2018-12-01 22:29:41 +0100

    Fix module export issue

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const msToTimecode = require("ms-to-timecode");
 const timecode = require("timecode").Timecode;
 
-const convert = (ms, fr) => {
+odule.exports.convert = (ms, fr) => {
 	const timecodeString = msToTimecode(ms, fr);
 	const tc = timecode.init({ framerate: fr, timecode: timecodeString });
 	const converted = tc.frame_count;
 	return converted;
 };
-
-module.exports = convert;
